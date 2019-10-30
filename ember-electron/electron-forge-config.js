0eda40fcabfd32816600af1a51857c6c51b68676
Robert Wagner <rwwagner90@gmail.com>
2019-10-29 22:06:45 -0400

    Initial test menu bar app

---
+++ ember-electron/electron-forge-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "make_targets": {
+    "win32": [
+      "squirrel"
+    ],
+    "darwin": [
+      "zip"
+    ],
+    "linux": [
+      "deb",
+      "rpm"
+    ]
+  },
+  "electronPackagerConfig": {
+    "packageManager": "yarn"
+  },
+  "electronWinstallerConfig": {
+    "name": "tinct"
+  },
+  "electronInstallerDebian": {},
+  "electronInstallerRedhat": {},
+  "github_repository": {
+    "owner": "",
+    "name": ""
+  },
+  "windowsStoreConfig": {
+    "packageName": "",
+    "name": "tinct"
+  }
+};

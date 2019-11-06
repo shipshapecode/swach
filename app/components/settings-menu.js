bdd595b3e134a5215a582200760e6346de78d0ce
Robert Wagner <rwwagner90@gmail.com>
2019-11-06 10:49:41 -0500

    Make settings a glimmer component

--- app/components/settings-menu.js
+++ app/components/settings-menu.js
@@ -1,21 +1,14 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { action, set } from '@ember/object';
-import { tagName } from '@ember-decorators/component';
 import { storageFor } from 'ember-local-storage';
 
-@tagName('')
 export default class SettingsMenu extends Component {
   @storageFor('settings') settings;
 
-  init() {
-    super.init(...arguments);
-
-    set(this, 'themes', ['dynamic', 'light', 'dark']);
-  }
+  themes = ['dynamic', 'light', 'dark'];
 
   @action
   changeTheme(theme) {
-    this.set('settings.userTheme', theme);
+    set(this, 'settings.userTheme', theme);
   }
 }
-

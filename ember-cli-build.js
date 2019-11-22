6f42e8507901c38ee98a900fc62b9038013127a9
Robert Wagner <rwwagner90@gmail.com>
2019-11-22 10:02:48 -0500

    Remove PurgeCSS for now

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,19 +1,21 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const isProduction = EmberApp.env() === 'production';
 
-const purgeCSS = {
-  module: require('@fullhuman/postcss-purgecss'),
-  options: {
-    content: [
-      // add extra paths here for components/controllers which include tailwind classes
-      './app/index.html',
-      './app/templates/**/*.hbs'
-    ],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-  }
-};
+// TODO: Figure out why PurgeCSS doesn't work with new ember-electron
+// const isProduction = EmberApp.env() === 'production';
+//
+// const purgeCSS = {
+//   module: require('@fullhuman/postcss-purgecss'),
+//   options: {
+//     content: [
+//       // add extra paths here for components/controllers which include tailwind classes
+//       './app/index.html',
+//       './app/templates/**/*.hbs'
+//     ],
+//     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//   }
+// };
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -26,8 +28,8 @@ module.exports = function(defaults) {
               path: ['node_modules']
             }
           },
-          require('tailwindcss')('./tailwind.config.js'),
-          ...isProduction ? [purgeCSS] : []
+          require('tailwindcss')('./tailwind.config.js')
+          // ...isProduction ? [purgeCSS] : []
         ]
       }
     }

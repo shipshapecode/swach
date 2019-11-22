bd7b62826a80e81ea171d670c21cff9522fce338
Robert Wagner <rwwagner90@gmail.com>
2019-11-21 21:18:47 -0500

    Set to loading html for before loadUrl

--- electron-app/src/index.js
+++ electron-app/src/index.js
@@ -4,6 +4,7 @@ const protocolServe = require('electron-protocol-serve');
 const { menubar } = require('menubar');
 
 const mb = menubar({
+  dir: join(__dirname || resolve(dirname('')), '..', 'loading'),
   browserWindow: {
     height: 600,
     width: 300,

9683709c0b066d59ad79629164332ecc491c2fbc
Robert Wagner <rwwagner90@gmail.com>
2019-11-24 22:01:47 -0500

    Start adding more settings

--- app/storages/settings.js
+++ app/storages/settings.js
@@ -2,4 +2,14 @@ import StorageObject from 'ember-local-storage/local/object';
 
 const Storage = StorageObject.extend();
 
+Storage.reopenClass({
+  initialState() {
+    return {
+      openOnStartup: false,
+      sounds: true,
+      userTheme: 'dynamic'
+    };
+  }
+});
+
 export default Storage;

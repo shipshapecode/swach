6df29ca14a307a409078946a4aab0d8af6add11e
Robert Wagner <rwwagner90@gmail.com>
2019-11-25 22:29:34 -0500

    Allow editing palette name

--- app/components/palette-row.js
+++ app/components/palette-row.js
@@ -1,10 +1,29 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
+import { action, set } from '@ember/object';
 
 export default class PaletteRowComponent extends Component {
+  isEditing = false;
+
   @action
   addColorToPalette(color, ops) {
     const palette = ops.target.palette;
     palette.colors.pushObject(color);
+    palette.save();
+  }
+
+  @action
+  insertedNameInput(element) {
+    element.focus();
+  }
+
+  @action
+  toggleIsEditing() {
+    set(this, 'isEditing', !this.isEditing);
+  }
+
+  @action
+  updatePaletteName(palette) {
+    palette.save();
+    set(this, 'isEditing', false);
   }
 }

ca69a7aa0b9cb9a1648a56e804005fe73ebc6ebd
Robert Wagner <rwwagner90@gmail.com>
2019-12-01 16:12:30 -0500

    Fix mirage associations

--- mirage/scenarios/shared.js
+++ mirage/scenarios/shared.js
@@ -6,25 +6,28 @@ export default function(server) {
 
   // server.createList('post', 10);
 
-  // const black = server.create('color', { hex: '#000000', name: 'Black' });
-  // const denim = server.create('color', { hex: '#356dc4', name: 'Denim' });
-  // const inchWorm = server.create('color', {
-  //   hex: '#b0f566',
-  //   name: 'Inch Worm'
-  // });
-  // const paleMagenta = server.create('color', {
-  //   hex: '#f78ae0',
-  //   name: 'Pale Magenta'
-  // });
-  // const shamrock = server.create('color', { hex: '#4af2a1', name: 'Shamrock' });
-  // const white = server.create('color', { hex: '#ffffff', name: 'White' });
+  const black = server.create('color', { hex: '#000000', name: 'Black' });
+  const denim = server.create('color', { hex: '#356dc4', name: 'Denim' });
+  const inchWorm = server.create('color', {
+    hex: '#b0f566',
+    name: 'Inch Worm'
+  });
+  const paleMagenta = server.create('color', {
+    hex: '#f78ae0',
+    name: 'Pale Magenta'
+  });
+  const shamrock = server.create('color', {
+    hex: '#4af2a1',
+    name: 'Shamrock'
+  });
+  const white = server.create('color', { hex: '#ffffff', name: 'White' });
 
   const colorHistoryPalette = server.create('palette', {
     id: 'color-history-123',
     isColorHistory: true
   });
-  colorHistoryPalette.createColor({ hex: '#000000', name: 'Black' });
-  colorHistoryPalette.createColor({ hex: '#f78ae0', name: 'Pale Magenta' });
-  colorHistoryPalette.createColor({ hex: '#4af2a1', name: 'Shamrock' });
-  colorHistoryPalette.createColor({ hex: '#ffffff', name: 'White' });
+  colorHistoryPalette.update('colors', [black, paleMagenta, shamrock, white]);
+
+  const firstPalette = server.create('palette');
+  firstPalette.update('colors', [black, denim, inchWorm, white]);
 }

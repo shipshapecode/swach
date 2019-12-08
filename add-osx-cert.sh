18d23d47136e0b720dd08b06e7e90e9dc1b71ac2
Robert Wagner <rwwagner90@gmail.com>
2019-12-07 22:15:28 -0500

    Try adding password

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env sh
 
 KEY_CHAIN=build.keychain
+KEY_CHAIN_PASSWORD=password123
 CERTIFICATE_P12=certificate.p12
 
 # Recreate the certificate from the secure environment variable
 echo $CERTIFICATE_OSX_APPLICATION | base64 --decode > $CERTIFICATE_P12
 
 #create a keychain
-security create-keychain -p $KEY_CHAIN
+security create-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN
 
 # Make the keychain the default so identities are found
 security default-keychain -s $KEY_CHAIN
 
 # Unlock the keychain
-security unlock-keychain -p $KEY_CHAIN
+security unlock-keychain -p $KEY_CHAIN_PASSWORD $KEY_CHAIN
 
 security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple: -s -k $KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple: -s -k $KEY_CHAIN_PASSWORD $KEY_CHAIN
 
 # remove certs
 rm -fr *.p12

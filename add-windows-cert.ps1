86dffe2efabf85b621c5f93dfe5a1d24fb4ca017
Robert Wagner <rwwagner90@gmail.com>
2019-12-19 18:23:10 -0500

    Try importing cert

--- add-windows-cert.ps1
+++ add-windows-cert.ps1
@@ -1,4 +1,8 @@
 $base64string = $Env:CERTIFICATE_WINDOWS_PFX
 $FileName = 'certificate.pfx'
+$Pfxpath = './certificate.pfx'
 
 [IO.File]::WriteAllBytes($FileName, [Convert]::FromBase64String($base64string))
+
+$Password = ConvertTo-SecureString -String $Env:WINDOWS_PFX_PASSWORD -AsPlainText -Force
+Import-PfxCertificate -FilePath $Pfxpath -CertStoreLocation Cert:\CurrentUser\My -Password $Password

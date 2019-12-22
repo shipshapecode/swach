ed23352a5c77503ad06b86dd9dfe7ec9c4a0c761
Robert Wagner <rwwagner90@gmail.com>
2019-12-22 14:32:59 -0500

    Add animation for favorites filtering

--- app/components/palettes-list.js
+++ app/components/palettes-list.js
@@ -1,4 +1,20 @@
 import Component from '@glimmer/component';
+import { fadeOut } from 'ember-animated/motions/opacity';
+import move from 'ember-animated/motions/move';
 
 export default class PalettesListComponent extends Component {
+  *transition({ keptSprites, insertedSprites, removedSprites }) {
+    for (let sprite of insertedSprites) {
+      sprite.startTranslatedBy(0, -sprite.finalBounds.height / 2);
+      move(sprite);
+    }
+
+    for (let sprite of keptSprites) {
+      move(sprite);
+    }
+
+    for (let sprite of removedSprites) {
+      fadeOut(sprite);
+    }
+  }
 }

e3f95122161bf4dbfe8c018a9cefc0daa6f6d522
Robert Wagner <rwwagner90@gmail.com>
2019-12-22 14:51:00 -0500

    Remove unused script

--- add-windows-cert.ps1
+++
@@ -1,8 +0,0 @@
-$base64string = $Env:CERTIFICATE_WINDOWS_PFX
-$FileName = 'win-certificate.pfx'
-$Pfxpath = './win-certificate.pfx'
-
-[IO.File]::WriteAllBytes($FileName, [Convert]::FromBase64String($base64string))
-
-$Password = ConvertTo-SecureString -String $Env:WINDOWS_PFX_PASSWORD -AsPlainText -Force
-Import-PfxCertificate -FilePath $Pfxpath -CertStoreLocation Cert:\CurrentUser\My -Password $Password

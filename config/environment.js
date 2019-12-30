da326b5286e6a49737a5fc6a3c22aaf634dc036e
Robert Wagner <rwwagner90@gmail.com>
2019-12-30 12:05:07 -0500

    Remove CSP for now

--- config/environment.js
+++ config/environment.js
@@ -22,21 +22,22 @@ module.exports = function(environment) {
       // when it is created
     },
 
-    contentSecurityPolicy: {
-      'default-src': ["'none'"],
-      'script-src': [
-        'http://localhost:7020',
-        "'self'",
-        "'unsafe-eval'",
-        "'unsafe-inline'"
-      ],
-      'font-src': ["'self'"],
-      'connect-src': ["'self'"],
-      'img-src': ['data:', "'self'"],
-      'style-src': ["'self'", "'unsafe-inline'"],
-      'media-src': ["'self'"]
-    },
-    contentSecurityPolicyMeta: true
+    // TODO: add CSP back. We need to figure out why it is making tests fail.
+    // contentSecurityPolicy: {
+    //   'default-src': ["'none'"],
+    //   'script-src': [
+    //     'http://localhost:7020',
+    //     "'self'",
+    //     "'unsafe-eval'",
+    //     "'unsafe-inline'"
+    //   ],
+    //   'font-src': ["'self'"],
+    //   'connect-src': ["'self'"],
+    //   'img-src': ['data:', "'self'"],
+    //   'style-src': ["'self'", "'unsafe-inline'"],
+    //   'media-src': ["'self'"]
+    // },
+    // contentSecurityPolicyMeta: true
   };
 
   if (environment === 'development') {

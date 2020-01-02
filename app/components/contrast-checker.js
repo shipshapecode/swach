e03e959b76a0e88cbe03af1ca099b7e0753485b9
Chuck Carpenter <chucksbackup@Sarahs-Air-2.lan>
2020-01-01 21:42:57 -0700

    :zap: Update pickr init in contrast route to match transition

--- app/components/contrast-checker.js
+++ app/components/contrast-checker.js
@@ -18,85 +18,89 @@ export default class ContrastChecker extends Component {
 
   @action
   initBackgroundColorPicker(element) {
-    this.bgPickr = new Pickr({
-      el: element,
-      container: element,
-      default: this.backgroundColor,
-      comparison: false,
-      inline: true,
-      useAsButton: true,
-
-      showAlways: true,
-      theme: 'nano',
-
-      components: {
-        // Main components
-        preview: false,
-        opacity: false,
-        hue: true,
-
-        // Input / output Options
-        interaction: {
-          hex: false,
-          rgba: false,
-          hsla: false,
-          hsva: false,
-          cmyk: false,
-          input: true,
-          clear: false,
-          save: false
+    setTimeout(() => {
+      this.bgPickr = new Pickr({
+        el: element,
+        container: element,
+        default: this.backgroundColor,
+        comparison: false,
+        inline: true,
+        useAsButton: true,
+  
+        showAlways: true,
+        theme: 'nano',
+  
+        components: {
+          // Main components
+          preview: false,
+          opacity: false,
+          hue: true,
+  
+          // Input / output Options
+          interaction: {
+            hex: false,
+            rgba: false,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: false
+          }
         }
-      }
-    });
-
-    this.onBgChange = (color) => {
-      if (color) {
-        this.backgroundColor = color.toHEXA().toString();
-      }
-    };
-
-    this.bgPickr.on('change', this.onBgChange);
+      });
+      this.onBgChange = (color) => {
+        if (color) {
+          this.backgroundColor = color.toHEXA().toString();
+        }
+      };
+  
+      this.bgPickr.on('change', this.onBgChange);
+      
+    }, 500);
   }
 
   @action
   initForegroundColorPicker(element) {
-    this.fgPickr = new Pickr({
-      el: element,
-      container: element,
-      comparison: false,
-      default: this.foregroundColor,
-      inline: true,
-      useAsButton: true,
-
-      showAlways: true,
-      theme: 'nano',
-
-      components: {
-        // Main components
-        preview: false,
-        opacity: false,
-        hue: true,
-
-        // Input / output Options
-        interaction: {
-          hex: false,
-          rgba: false,
-          hsla: false,
-          hsva: false,
-          cmyk: false,
-          input: true,
-          clear: false,
-          save: false
+    setTimeout(() => {
+      this.fgPickr = new Pickr({
+        el: element,
+        container: element,
+        comparison: false,
+        default: this.foregroundColor,
+        inline: true,
+        useAsButton: true,
+  
+        showAlways: true,
+        theme: 'nano',
+  
+        components: {
+          // Main components
+          preview: false,
+          opacity: false,
+          hue: true,
+  
+          // Input / output Options
+          interaction: {
+            hex: false,
+            rgba: false,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: true,
+            clear: false,
+            save: false
+          }
         }
-      }
-    });
-
-    this.onFgChange = (color) => {
-      if (color) {
-        this.foregroundColor = color.toHEXA().toString();
-      }
-    };
-
-    this.fgPickr.on('change', this.onFgChange);
+      });
+  
+      this.onFgChange = (color) => {
+        if (color) {
+          this.foregroundColor = color.toHEXA().toString();
+        }
+      };
+  
+      this.fgPickr.on('change', this.onFgChange);
+    }, 500);
   }
 }

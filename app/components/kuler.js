12b0267ecc863b2b131aa57dae7b817d956cc16a
Robert Wagner <rwwagner90@gmail.com>
2020-01-02 15:17:35 -0500

    Show list of harmonies

--- app/components/kuler.js
+++ app/components/kuler.js
@@ -4,42 +4,68 @@ import { inject as service } from '@ember/service';
 import { capitalize } from '@ember/string';
 import { tracked } from '@glimmer/tracking';
 import { TinyColor } from '@ctrl/tinycolor';
+import { fadeOut } from 'ember-animated/motions/opacity';
+import move from 'ember-animated/motions/move';
 
 export default class KulerComponent extends Component {
   @service colorUtils;
   @service store;
 
   harmonies = ['analogous', 'monochromatic', 'tetrad', 'triad'];
+  @tracked palettes = [];
 
-  @tracked palette;
-  @tracked selectedHarmony;
+  constructor() {
+    super(...arguments);
 
-  willDestroy() {
-    if (this.palette && this.palette.isNew) {
-      this.palette.destroyRecord();
-    }
+    this.baseColorChanged();
   }
 
   @action
-  async setSelectedHarmony(selectedHarmony) {
-    this.selectedHarmony = selectedHarmony;
+  async baseColorChanged() {
+    await this._destroyLeftoverPalettes();
 
-    if (this.selectedHarmony) {
-      if (this.palette && this.palette.isNew) {
-        await this.palette.destroyRecord();
-      }
-      this.palette = await this.store.createRecord('palette', {
-        name: capitalize(this.selectedHarmony)
+    for (const harmony of this.harmonies) {
+      const palette = this.store.createRecord('palette', {
+        name: capitalize(harmony)
       });
 
-      let colors = new TinyColor(this.args.baseColor.hex)[this.selectedHarmony](
-        5
-      );
+      let colors = new TinyColor(this.args.baseColor.hex)[harmony](5);
       colors = colors.map(color =>
         this.colorUtils.createColorRecord(color.toHexString())
       );
 
-      this.palette.colors.pushObjects(colors);
+      palette.colors.pushObjects(colors);
+
+      this.palettes.pushObject(palette);
+    }
+  }
+
+  willDestroy() {
+    this._destroyLeftoverPalettes();
+  }
+
+  async _destroyLeftoverPalettes() {
+    for (const palette of this.palettes) {
+      if (palette.isNew) {
+        await palette.destroyRecord();
+      }
+    }
+
+    this.palettes = [];
+  }
+
+  *transition({ keptSprites, insertedSprites, removedSprites }) {
+    for (let sprite of insertedSprites) {
+      sprite.startTranslatedBy(0, -sprite.finalBounds.height / 2);
+      move(sprite);
+    }
+
+    for (let sprite of keptSprites) {
+      move(sprite);
+    }
+
+    for (let sprite of removedSprites) {
+      fadeOut(sprite);
     }
   }
 }

857a22b4821d575d6ec4b2b8aecd3939bf609c75
Robert Wagner <rwwagner90@gmail.com>
2020-01-02 20:48:23 -0500

    Add ability to save palette

--- app/components/kuler-palette-row.js
+++ app/components/kuler-palette-row.js
@@ -1,6 +1,19 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import fade from 'ember-animated/transitions/fade';
+import { action } from '@ember/object';
 
 export default class KulerPaletteRowComponent extends Component {
   @service colorUtils;
+  @service router;
+
+  fade = fade;
+  showMenu = false;
+
+  @action
+  async savePalette() {
+    await this.router.transitionTo('palettes');
+    await this.args.palette.colors.invoke('save');
+    await this.args.palette.save();
+  }
 }

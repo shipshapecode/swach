f3e1d89be1abe107db18e2f20290d6cbdfb616d2
Robert Wagner <rwwagner90@gmail.com>
2020-01-04 08:51:12 -0500

    Add tests, bump deps

--- tests/acceptance/kuler-test.js
+++ tests/acceptance/kuler-test.js
@@ -18,8 +18,95 @@ module('Acceptance | kuler', function(hooks) {
     assert.equal(currentURL(), '/kuler?colorId=color-1');
   });
 
-  test('shows all color harmonies', async function(assert) {
-    assert.dom('[data-test-kuler-palette-options]').exists({ count: 17 });
-    assert.dom('[data-test-kuler-palette]').exists();
+  test('analogous palette', async function(assert) {
+    assert
+    .dom(
+      '[data-test-kuler-palette="Analogous"] [data-test-kuler-palette-name]'
+    )
+    .hasText('Analogous');
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Analogous"] [data-test-kuler-palette-color]'
+      )
+      .exists({ count: 5 });
+  });
+
+  test('monochromatic palette', async function(assert) {
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-name]'
+      )
+      .hasText('Monochromatic');
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-color]'
+      )
+      .exists({ count: 5 });
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-color="0"]'
+      )
+      .hasStyle({
+        backgroundColor: 'rgb(247, 138, 224)'
+      });
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-color="1"]'
+      )
+      .hasStyle({
+        backgroundColor: 'rgb(43, 24, 39)'
+      });
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-color="2"]'
+      )
+      .hasStyle({
+        backgroundColor: 'rgb(94, 53, 85)'
+      });
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-color="3"]'
+      )
+      .hasStyle({
+        backgroundColor: 'rgb(145, 81, 131)'
+      });
+
+    assert
+      .dom(
+        '[data-test-kuler-palette="Monochromatic"] [data-test-kuler-palette-color="4"]'
+      )
+      .hasStyle({
+        backgroundColor: 'rgb(196, 110, 178)'
+      });
+  });
+
+  test('tetrad palette', async function(assert) {
+    assert
+    .dom(
+      '[data-test-kuler-palette="Tetrad"] [data-test-kuler-palette-name]'
+    )
+    .hasText('Tetrad');
+
+    assert
+      .dom('[data-test-kuler-palette="Tetrad"] [data-test-kuler-palette-color]')
+      .exists({ count: 4 });
+  });
+
+  test('triad palette', async function(assert) {
+    assert
+    .dom(
+      '[data-test-kuler-palette="Triad"] [data-test-kuler-palette-name]'
+    )
+    .hasText('Triad');
+
+    assert
+      .dom('[data-test-kuler-palette="Triad"] [data-test-kuler-palette-color]')
+      .exists({ count: 3 });
   });
 });

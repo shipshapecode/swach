851b1096a21897c86b38e835a0113f1ef0ec6ac5
Robert Wagner <rwwagner90@gmail.com>
2020-01-22 15:27:02 -0500

    Support undo/redo for deleteColor

--- app/components/colors-list.js
+++ app/components/colors-list.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { fadeOut } from 'ember-animated/motions/opacity';
 import move from 'ember-animated/motions/move';
 import { easeOut } from 'ember-animated/easings/cosine';
 
 export default class ColorsList extends Component {
+  @service store;
+  @service undoManager;
+
   get sortedColors() {
     return this.args.palette.colors.sortBy('createdAt').reverse();
   }
@@ -28,11 +32,26 @@ export default class ColorsList extends Component {
   async deleteColor(color) {
     const { palette } = this.args;
     if (!palette.isLocked) {
-      palette.colors.removeObject(color);
+      await palette.colors.removeObject(color);
 
-      if (!color.palettes.length) {
-        await color.remove();
-      }
+      const transformId = this.store.transformLog.head;
+      const redoTransform = this.store.getTransform(transformId).operations;
+      const undoTransform = this.store.getInverseOperations(transformId);
+  
+      const undo = async () => {
+        await this.store.update(undoTransform);
+      };
+  
+      const redo = async () => {
+        await this.store.update(redoTransform);
+      };
+  
+      this.undoManager.add({ undo, redo });
+
+      // TODO: figure out how to add this back and be able to undo/redo
+      // if (!color.palettes.length) {
+      //   await color.remove();
+      // }
     }
   }
 }

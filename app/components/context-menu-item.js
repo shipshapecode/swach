91ecc96aae1c5a4772cbefca0d0e7b51a02f29ce
Robert Wagner <rwwagner90@gmail.com>
2020-02-09 13:26:17 -0500

    Allow selecting colors in kuler row (#36)
    * Allow selecting colors in kuler row
    
    * Fix delete palette / undo, context-menu-item glimmer
    
    * Fix lint

--- app/components/context-menu-item.js
+++ app/components/context-menu-item.js
@@ -1,3 +1,24 @@
-import ContextMenuItem from 'ember-context-menu/components/context-menu-item';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
-export default class ExtendedContextMenuItem extends ContextMenuItem {}
+export default class ContextMenuItemComponent extends Component {
+  get _amount() {
+    return !this._isParent && this.args.amount > 1 && this.args.amount;
+  }
+
+  get _isParent() {
+    const { subActions } = this.args.item;
+    return subActions ? subActions.length : 0;
+  }
+
+  get isDisabled() {
+    return this.args.itemIsDisabled(this.args.item);
+  }
+
+  @action
+  itemClicked() {
+    if (!this.isDisabled && !this._isParent) {
+      this.args.clickAction(this.args.item);
+    }
+  }
+}

c231cc1eadbdce28559a07ccf45bc3297565a854
Robert Wagner <rwwagner90@gmail.com>
2020-02-09 09:28:23 -0500

    Remove purgeCSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,27 +2,10 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-// TODO: Figure out why PurgeCSS doesn't work with new ember-electron
-// const isProduction = EmberApp.env() === 'production';
-//
-// const purgeCSS = {
-//   module: require('@fullhuman/postcss-purgecss'),
-//   options: {
-//     content: [
-//       // add extra paths here for components/controllers which include tailwind classes
-//       './app/index.html',
-//       './app/templates/**/*.hbs'
-//     ],
-//     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-//   }
-// };
-
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     babel: {
-      plugins: [
-        '@babel/plugin-proposal-object-rest-spread'
-      ]
+      plugins: ['@babel/plugin-proposal-object-rest-spread']
     },
     postcssOptions: {
       compile: {
@@ -37,7 +20,6 @@ module.exports = function(defaults) {
           },
           require('tailwindcss')('./tailwind.config.js'),
           require('postcss-nested')
-          // ...isProduction ? [purgeCSS] : []
         ]
       }
     }

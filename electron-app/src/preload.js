c9894b79834c0b61028caaaadcccd7c150082048
Robert Wagner <rwwagner90@gmail.com>
2020-03-08 10:35:32 -0400

    Add Sentry to electron app

--- electron-app/src/preload.js
+++ electron-app/src/preload.js
@@ -1,4 +1,9 @@
 const { remote } = require('electron');
+const Sentry = require('@sentry/electron');
+    
+Sentry.init({
+  dsn: 'https://6974b46329f24dc1b9fca4507c65e942@sentry.io/3956140',
+});
 
 if (process.platform === 'darwin') {
   const { nativeTheme, systemPreferences } = remote;

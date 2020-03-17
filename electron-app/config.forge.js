3e63e923d42b371754b21b155c7df70e7043c23e
Robert Wagner <rwwagner90@gmail.com>
2020-03-17 13:10:04 -0400

    Update config.forge.js

--- electron-app/config.forge.js
+++ electron-app/config.forge.js
@@ -36,15 +36,17 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-zip',
-      platforms: ['darwin']
+      platforms: ['darwin', 'linux']
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {}
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {}
+      config: {
+        options: {
+          maintainer: 'Ship Shape Consulting LLC',
+          homepage: 'https://swach.io'
+        }
+      },
+      platforms: ['linux']
     }
   ]
 };

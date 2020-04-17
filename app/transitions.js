ba06612ae01e02a9491b6b6376c7adfa3dc040dc
Robert Wagner <rwwagner90@gmail.com>
2020-04-17 10:19:31 -0400

    Bump deps, speed up transitions

--- app/transitions.js
+++ app/transitions.js
@@ -1,33 +1,34 @@
-export default function() {
+const transitionOptions = { duration: 300, easing: 'easeInOut' };
+export default function () {
   this.transition(
     this.fromRoute('palettes'),
     this.toRoute('colors'),
-    this.use('toLeft', { easing: 'easeInOut' }),
-    this.reverse('toRight', { easing: 'easeInOut' })
+    this.use('toLeft', transitionOptions),
+    this.reverse('toRight', transitionOptions)
   );
 
   this.transition(
     this.fromRoute('colors'),
     this.toRoute('kuler'),
-    this.use('toLeft', { easing: 'easeInOut' }),
-    this.reverse('toRight', { easing: 'easeInOut' })
+    this.use('toLeft', transitionOptions),
+    this.reverse('toRight', transitionOptions)
   );
 
   this.transition(
     this.fromRoute('kuler'),
     this.toRoute('palettes'),
-    this.use('toRight', { easing: 'easeInOut' })
+    this.use('toRight', transitionOptions)
   );
 
   this.transition(
     this.toRoute('contrast'),
-    this.use('toLeft', { easing: 'easeInOut' }),
-    this.reverse('toRight', { easing: 'easeInOut' })
+    this.use('toLeft', transitionOptions),
+    this.reverse('toRight', transitionOptions)
   );
 
   this.transition(
     this.toRoute('settings'),
-    this.use('toLeft', { easing: 'easeInOut' }),
-    this.reverse('toRight', { easing: 'easeInOut' })
+    this.use('toLeft', transitionOptions),
+    this.reverse('toRight', transitionOptions)
   );
 }
--- electron-app/src/index.js
+++ electron-app/src/index.js
@@ -1,6 +1,5 @@
 const {
   clipboard,
-  globalShortcut,
   protocol,
   Menu,
   ipcMain

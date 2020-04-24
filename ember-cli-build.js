34d61f70b3c1581d23ab77ad641cd172d2caddc1
Robert Wagner <rwwagner90@gmail.com>
2020-04-24 12:27:16 -0400

    Add back purgeCSS

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,20 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/components/**/*.hbs',
+      './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
@@ -19,7 +33,8 @@ module.exports = function(defaults) {
             }
           },
           require('tailwindcss')('./tailwind.config.js'),
-          require('postcss-nested')
+          require('postcss-nested'),
+          ...isProduction ? [purgeCSS] : []
         ]
       }
     }

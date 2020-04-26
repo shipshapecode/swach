760acfb2ba36281bea9f49aa420fbd32aaa92f8a
Robert Wagner <rwwagner90@gmail.com>
2020-04-26 11:59:00 -0400

    Fix lint

--- app/components/settings-menu.js
+++ app/components/settings-menu.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import { action, get, set } from '@ember/object';
+import { action, set } from '@ember/object';
 import { storageFor } from 'ember-local-storage';
 
 export default class SettingsMenu extends Component {

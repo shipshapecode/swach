7b966d3e4b20e5475eb2563ed2e36ba4172d252d
Robert Wagner <rwwagner90@gmail.com>
2020-05-11 20:15:40 -0400

    Try browserSandbox: false

--- electron-app/config.forge.js
+++ electron-app/config.forge.js
@@ -62,6 +62,7 @@ module.exports = {
         description: 'A robust color management tool for the modern age.',
         features: {
           audio: true,
+          browserSandbox: false,
           webgl: true
         },
         grade: 'stable',

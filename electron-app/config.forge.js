d9ffece720b5b5419b99a24c983caa5622fab5bd
Robert Wagner <rwwagner90@gmail.com>
2020-05-12 08:03:00 -0400

    Try setting browser-support options

--- electron-app/config.forge.js
+++ electron-app/config.forge.js
@@ -58,6 +58,13 @@ module.exports = {
       name: '@electron-forge/maker-snap',
       platforms: ['linux'],
       config: {
+        appPlugs: ['browser-support'],
+        plugs: {
+          'browser-support': {
+            interface: 'browser-support',
+            'allow-sandbox': false
+          }
+        },
         confinement: 'strict',
         description: 'A robust color management tool for the modern age.',
         features: {

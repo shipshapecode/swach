e0421e6b730d45909933a0540bad9f182f9b8dc4
Robert Wagner <rwwagner90@gmail.com>
2020-05-14 23:10:44 -0400

    Back to browser-support

--- electron-app/config.forge.js
+++ electron-app/config.forge.js
@@ -48,28 +48,28 @@ module.exports = {
       platforms: ['linux'],
       config: {
         appConfig: { name: 'swach', title: 'Swach' },
-        // appPlugs: [
-        //   //'desktop',
-        //   // 'wayland',
-        //   // 'unity7',
-        //   // 'alsa',
-        //   // 'avahi-observe',
-        //   'browser-support'
-        //   // 'cups-control',
-        //   // 'gsettings',
-        //   // 'home',
-        //   // 'network',
-        //   // 'opengl',
-        //   // 'audio-playback',
-        //   // 'screen-inhibit-control',
-        //   // 'upower-observe'
-        // ],
-        // plugs: {
-        //   'browser-support': {
-        //     interface: 'browser-support',
-        //     'allow-sandbox': false
-        //   }
-        // },
+        appPlugs: [
+          //'desktop',
+          // 'wayland',
+          // 'unity7',
+          // 'alsa',
+          // 'avahi-observe',
+          'browser-support'
+          // 'cups-control',
+          // 'gsettings',
+          // 'home',
+          // 'network',
+          // 'opengl',
+          // 'audio-playback',
+          // 'screen-inhibit-control',
+          // 'upower-observe'
+        ],
+        plugs: {
+          'browser-support': {
+            interface: 'browser-support',
+            'allow-sandbox': false
+          }
+        },
         confinement: 'strict',
         description: 'A robust color management tool for the modern age.',
         features: {

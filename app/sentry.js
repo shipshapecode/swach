162fda490f4a66896aa8fbab228245a06d859a2e
Robert Wagner <rwwagner90@gmail.com>
2020-09-17 14:37:00 -0400

    Ensure requireNode exists

--- app/sentry.js
+++ app/sentry.js
@@ -1,27 +1,30 @@
-const Sentry = requireNode('@sentry/electron');
 import { Ember } from '@sentry/integrations/esm/ember';
 import { CaptureConsole } from '@sentry/integrations/esm/captureconsole';
 
 import config from './config/environment';
 
 export function startSentry() {
-  Sentry.init({
-    ...config.sentry,
-    integrations: [
-      new Ember(),
-      new CaptureConsole({
-        levels: ['error']
-      })
-    ],
-    beforeSend(event, hint) {
-      let error = hint.originalException;
+  if (typeof requireNode !== 'undefined') {
+    const Sentry = requireNode('@sentry/electron');
 
-      // ignore aborted route transitions from the Ember.js router
-      if (error && error.name === 'TransitionAborted') {
-        return null;
+    Sentry.init({
+      ...config.sentry,
+      integrations: [
+        new Ember(),
+        new CaptureConsole({
+          levels: ['error']
+        })
+      ],
+      beforeSend(event, hint) {
+        let error = hint.originalException;
+
+        // ignore aborted route transitions from the Ember.js router
+        if (error && error.name === 'TransitionAborted') {
+          return null;
+        }
+
+        return event;
       }
-
-      return event;
-    }
-  });
+    });
+  }
 }

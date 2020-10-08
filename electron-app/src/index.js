c815f0e6c0ea058b3654687e62f1c6289390fb93
Robert Wagner <rwwagner90@gmail.com>
2020-10-08 10:45:16 -0400

    Fix sentry and font issues

--- electron-app/src/index.js
+++ electron-app/src/index.js
@@ -35,8 +35,9 @@ if (isDev) {
 const Sentry = require('@sentry/electron');
 
 Sentry.init({
+  appName: 'swach',
   dsn: 'https://6974b46329f24dc1b9fca4507c65e942@sentry.io/3956140',
-  release: `v${app.getVersion()}`
+  release: `v${require('../package').version}`
 });
 
 const Store = require('electron-store');

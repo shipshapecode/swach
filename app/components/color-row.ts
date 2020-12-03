aeda9a4dfffa897aedcc8ddd5395e58ba7ad98b6
Robert Wagner <rwwagner90@gmail.com>
2020-12-03 11:33:02 -0500

    Start converting to TypeScript

--- app/components/color-row.ts
+++ app/components/color-row.ts
@@ -4,16 +4,25 @@ import { isEmpty } from '@ember/utils';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import fade from 'ember-animated/transitions/fade';
+import ColorUtils from 'swach/services/color-utils';
+import Router from '@ember/routing/router-service';
 
-export default class ColorRow extends Component {
-  @service colorUtils;
-  @service router;
+interface ColorRowArgs {
+  color: any;
+  deleteColor: Function;
+  palette: any;
+  showActions: boolean;
+}
+
+export default class ColorRow extends Component<ColorRowArgs>{
+  @service colorUtils!: ColorUtils;
+  @service router!: Router;
 
   @tracked deleteConfirm = false;
   fade = fade;
   showMenu = false;
 
-  get showActions() {
+  get showActions(): boolean {
     if (isEmpty(this.args.showActions)) {
       return true;
     }
@@ -22,7 +31,7 @@ export default class ColorRow extends Component {
   }
 
   @action
-  deleteColor(color) {
+  deleteColor(color: any) {
     if (!this.args.palette.isLocked) {
       if (this.deleteConfirm) {
         this.args.deleteColor(color);
@@ -33,7 +42,7 @@ export default class ColorRow extends Component {
   }
 
   @action
-  transitionToKuler(event) {
+  transitionToKuler(event: any) {
     event.stopPropagation();
     this.router.transitionTo('kuler', {
       queryParams: { colorId: this.args.color.id }

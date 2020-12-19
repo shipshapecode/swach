0f698541af3695082578b97383d9e5d9844a9570
Robert Wagner <rwwagner90@gmail.com>
2020-12-19 13:48:19 -0500

    Convert more controllers

--- app/controllers/settings.ts
+++ app/controllers/settings.ts
@@ -1,10 +1,12 @@
 import Controller, { inject as controller } from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import ApplicationController from 'swach/controllers/application';
+import Session from 'ember-simple-auth/services/session';
 
 export default class SettingsController extends Controller {
-  @controller application;
-  @service session;
+  @controller application!: ApplicationController;
+  @service session!: Session;
 
   @action
   goBack() {

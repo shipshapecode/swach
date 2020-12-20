2d656f958c5aae83ed16d94833a37d1e54a6045d
Robert Wagner <rwwagner90@gmail.com>
2020-12-20 13:10:56 -0500

    Palettes controller types

---
+++ app/controllers/palettes.ts
@@ -0,0 +1,236 @@
+import Controller, { inject as controller } from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import ApplicationController from 'swach/controllers/application';
+import { Store } from 'ember-orbit';
+import Router from '@ember/routing/router-service';
+import ColorUtils from 'swach/services/color-utils';
+import UndoManager from 'swach/services/undo-manager';
+import ColorModel from 'swach/data-models/color';
+import PaletteModel from 'swach/data-models/palette';
+
+export default class PalettesController extends Controller {
+  @controller application!: ApplicationController;
+  @service colorUtils!: ColorUtils;
+  @service router!: Router;
+  @service store!: Store;
+  @service undoManager!: UndoManager;
+
+  @tracked showFavorites = false;
+
+  get modelArray() {
+    return this.model.value;
+  }
+
+  get colorHistory() {
+    return this.modelArray.findBy('isColorHistory', true);
+  }
+
+  get last16Colors() {
+    const colors = (this.colorHistory && this.colorHistory.colors) || [];
+    return colors.sortBy('createdAt').reverse().slice(0, 16);
+  }
+
+  get palettes() {
+    let palettes = this.modelArray || [];
+
+    if (this.showFavorites) {
+      palettes = palettes.filterBy('isFavorite', true);
+    }
+
+    return palettes.filterBy('isColorHistory', false);
+  }
+
+  @action
+  async createNewPalette() {
+    await this.store.addRecord({
+      type: 'palette',
+      name: 'Palette',
+      createdAt: new Date(),
+      colorOrder: [],
+      isColorHistory: false,
+      isFavorite: false,
+      isLocked: false
+    });
+
+    this.undoManager.setupUndoRedo();
+  }
+
+  @action
+  disableMovingColors({
+    draggedItem,
+    items
+  }: {
+    draggedItem: ColorModel;
+    items: ColorModel[];
+  }) {
+    return items.indexOf(draggedItem);
+  }
+
+  @action
+  async moveColorsBetweenPalettes({
+    sourceArgs,
+    sourceList,
+    sourceIndex,
+    targetArgs,
+    targetList,
+    targetIndex
+  }: {
+    sourceArgs: { isColorHistory: boolean; parent: PaletteModel };
+    sourceList: ColorModel[];
+    sourceIndex: number;
+    targetArgs: { isColorHistory: boolean; parent: PaletteModel };
+    targetList: ColorModel[];
+    targetIndex: number;
+  }) {
+    // If the same list and same index, we are not moving anywhere, so return
+    if (sourceList === targetList && sourceIndex === targetIndex) return;
+
+    const sourceParent = sourceArgs.parent;
+    const targetParent = targetArgs.parent;
+
+    // If the palette is locked, we should not allow dragging colors into or out of it
+    if (
+      (sourceParent && sourceParent.isLocked) ||
+      (targetParent && targetParent.isLocked)
+    )
+      return;
+
+    const item = sourceList.objectAt(sourceIndex);
+
+    if (item) {
+      // Dragging color out of color history
+      if (sourceArgs.isColorHistory) {
+        if (sourceList !== targetList) {
+          const colorsList = targetList.map((color) => {
+            return { type: 'color', id: color.id };
+          });
+
+          const existingColor = targetList.findBy('hex', item.hex);
+          if (existingColor) {
+            const colorToRemove = colorsList.findBy('id', existingColor.id);
+            if (colorToRemove) {
+              colorsList.removeObject(colorToRemove);
+            }
+          }
+
+          colorsList.insertAt(targetIndex, {
+            type: 'color',
+            id: item.id
+          });
+
+          await this.store.update((t) => {
+            const operations = [];
+
+            operations.push(
+              t.replaceAttribute(
+                { type: 'palette', id: targetParent.id },
+                'colorOrder',
+                colorsList
+              )
+            );
+
+            operations.push(
+              t.replaceRelatedRecords(
+                { type: 'palette', id: targetParent.id },
+                'colors',
+                colorsList
+              )
+            );
+
+            return operations;
+          });
+        }
+      } else {
+        const sourceColor = sourceList.objectAt(sourceIndex);
+
+        if (sourceColor) {
+          const sourceColorList = sourceList.map((color: ColorModel) => {
+            return { type: 'color', id: color.id };
+          });
+
+          const colorToRemove = sourceColorList.findBy('id', sourceColor.id);
+
+          if (colorToRemove) {
+            sourceColorList.removeObject(colorToRemove);
+
+            await this.store.update((t) => {
+              const operations = [];
+
+              operations.push(
+                t.removeFromRelatedRecords(
+                  { type: 'palette', id: sourceParent.id },
+                  'colors',
+                  { type: 'color', id: sourceColor.id }
+                )
+              );
+
+              operations.push(
+                t.replaceAttribute(
+                  { type: 'palette', id: sourceParent.id },
+                  'colorOrder',
+                  sourceColorList
+                )
+              );
+
+              if (!targetArgs.isColorHistory) {
+                const targetColorsList = targetList.map((color: ColorModel) => {
+                  return { type: 'color', id: color.id };
+                });
+
+                const existingColor = targetList.findBy('hex', item.hex);
+                if (existingColor) {
+                  const colorToRemove = targetColorsList.findBy(
+                    'id',
+                    existingColor.id
+                  );
+                  targetColorsList.removeObject(colorToRemove);
+
+                  t.removeFromRelatedRecords(
+                    { type: 'palette', id: targetParent.id },
+                    'colors',
+                    { type: 'color', id: existingColor.id }
+                  );
+                }
+
+                targetColorsList.insertAt(targetIndex, {
+                  type: 'color',
+                  id: item.id
+                });
+
+                operations.push(
+                  t.replaceAttribute(
+                    { type: 'palette', id: targetParent.id },
+                    'colorOrder',
+                    targetColorsList
+                  )
+                );
+
+                operations.push(
+                  t.addToRelatedRecords(
+                    { type: 'palette', id: targetParent.id },
+                    'colors',
+                    { type: 'color', id: item.id }
+                  )
+                );
+              }
+
+              return operations;
+            });
+          }
+        }
+      }
+
+      this.undoManager.setupUndoRedo();
+    }
+  }
+
+  @action
+  transitionToColorHistory(event: InputEvent) {
+    event.stopPropagation();
+    this.router.transitionTo('colors', {
+      queryParams: { paletteId: this.colorHistory.id }
+    });
+  }
+}

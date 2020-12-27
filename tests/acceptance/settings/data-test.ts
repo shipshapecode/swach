2e3e109da63fc19195ef8de43e041fad4e303f67
Robert Wagner <rwwagner90@gmail.com>
2020-12-27 12:07:48 -0500

    Add download success / error flash message

--- tests/acceptance/settings/data-test.ts
+++ tests/acceptance/settings/data-test.ts
@@ -1,17 +1,23 @@
-import { currentURL, visit } from '@ember/test-helpers';
+import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
 import resetStorages from 'ember-local-storage/test-support/reset-storage';
 
+import IDBExportImport from 'indexeddb-export-import';
+import sinon from 'sinon';
+
+import * as utils from 'swach/utils/get-db-open-request';
 import seedOrbit from 'swach/tests/orbit/seed';
+import { waitForAll } from 'swach/tests/helpers';
 
 module('Acceptance | settings/data', function (hooks) {
   setupApplicationTest(hooks);
 
   hooks.beforeEach(async function () {
+    sinon.stub(utils, 'getDBOpenRequest').returns({ result: {} });
     await seedOrbit(this.owner);
-    await visit('/settings');
+    await visit('/settings/data');
   });
 
   hooks.afterEach(function () {
@@ -19,8 +25,23 @@ module('Acceptance | settings/data', function (hooks) {
   });
 
   test('visiting /settings/data', async function (assert) {
-    await visit('/settings/data');
-
     assert.equal(currentURL(), '/settings/data');
   });
+
+  // Electron specific tests
+  if (typeof requireNode !== 'undefined') {
+    test('export triggers success message', async function (assert) {
+      sinon.stub(IDBExportImport, 'exportToJsonString').callsArg(1);
+      await click('[data-test-export-swatches-button]');
+      await waitForAll();
+      assert.dom('.alert.alert-success').exists({ count: 1 });
+    });
+
+    test('export triggers error message', async function (assert) {
+      sinon.stub(IDBExportImport, 'exportToJsonString').callsArgWith(1, 'error')
+      await click('[data-test-export-swatches-button]');
+      await waitForAll();
+      assert.dom('.alert.alert-danger').exists({ count: 1 });
+    });
+  }
 });
--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -3,11 +3,17 @@ import { start } from 'ember-qunit';
 import * as QUnit from 'qunit';
 import { setup } from 'qunit-dom';
 
+import setupSinon from 'ember-sinon-qunit';
+
 import Application from 'swach/app';
 import config from 'swach/config/environment';
+
+import './helpers/flash-message';
 
 setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
+
+setupSinon();
 
 start();

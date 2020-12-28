c0951fd49241cd14085c41bdd19c956633086620
Robert Wagner <rwwagner90@gmail.com>
2020-12-27 19:30:42 -0500

    More component types

---
+++ types/ember-drag-sort/components/drag-sort-list.d.ts
@@ -0,0 +1,7 @@
+declare module 'ember-drag-sort/components/drag-sort-list' {
+  import EmberObject from '@ember/object';
+
+  export default class DragSortList extends EmberObject {
+    dragEnter(): void {}
+  }
+}

9efc7e5369456611b7db774462962eea7758ad5c
Robert Wagner <rwwagner90@gmail.com>
2021-01-18 20:57:43 -0500

    Fix lint

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     'no-implicit-this': { allow: ['context-menu'] },
     'no-inline-styles': false,
     'no-invalid-interactive': false,
+    'no-negated-condition': false,
     'no-outlet-outside-routes': false
   }
 };

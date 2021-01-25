c02494854c1a69eb503be3b720018862a5a0c1dd
Robert Wagner <rwwagner90@gmail.com>
2021-01-24 23:25:32 -0500

    Use embroider

--- tests/acceptance/settings/data-test.ts
+++ tests/acceptance/settings/data-test.ts
@@ -1,13 +1,13 @@
-import { currentURL, visit } from '@ember/test-helpers';
+import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
 import resetStorages from 'ember-local-storage/test-support/reset-storage';
 
-// import IDBExportImport from 'indexeddb-export-import';
+import IDBExportImport from 'indexeddb-export-import';
 import sinon from 'sinon';
 
-// import { waitForAll } from 'swach/tests/helpers';
+import { waitForAll } from 'swach/tests/helpers';
 import seedOrbit from 'swach/tests/orbit/seed';
 import * as utils from 'swach/utils/get-db-open-request';
 
@@ -31,20 +31,19 @@ module('Acceptance | settings/data', function (hooks) {
 
   // Electron specific tests
   if (typeof requireNode !== 'undefined') {
-    // TODO: we need an embroider fix before we can run these tests
-    // test('export triggers success message', async function (assert) {
-    //   sinon.stub(IDBExportImport, 'exportToJsonString').callsArg(1);
-    //   await click('[data-test-export-swatches-button]');
-    //   await waitForAll();
-    //   assert.dom('.alert.alert-success').exists({ count: 1 });
-    // });
-    // test('export triggers error message', async function (assert) {
-    //   sinon
-    //     .stub(IDBExportImport, 'exportToJsonString')
-    //     .callsArgWith(1, 'error');
-    //   await click('[data-test-export-swatches-button]');
-    //   await waitForAll();
-    //   assert.dom('.alert.alert-danger').exists({ count: 1 });
-    // });
+    test('export triggers success message', async function (assert) {
+      sinon.stub(IDBExportImport, 'exportToJsonString').callsArg(1);
+      await click('[data-test-export-swatches-button]');
+      await waitForAll();
+      assert.dom('.alert.alert-success').exists({ count: 1 });
+    });
+    test('export triggers error message', async function (assert) {
+      sinon
+        .stub(IDBExportImport, 'exportToJsonString')
+        .callsArgWith(1, 'error');
+      await click('[data-test-export-swatches-button]');
+      await waitForAll();
+      assert.dom('.alert.alert-danger').exists({ count: 1 });
+    });
   }
 });

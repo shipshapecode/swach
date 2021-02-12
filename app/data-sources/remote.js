1ca6050831254f813e7997a7ff574ff268190689
Robert Wagner <rwwagner90@gmail.com>
2021-02-12 16:12:45 -0500

    Start API integration

---
+++ app/data-sources/remote.js
@@ -0,0 +1,36 @@
+import { getOwner } from '@ember/application';
+import { isPresent } from '@ember/utils';
+
+import RemoteSource, { JSONAPIRequestProcessor } from '@orbit/jsonapi';
+
+export default {
+  create(injections = {}) {
+    class RemoteRequestProcessor extends JSONAPIRequestProcessor {
+      initFetchSettings(customSettings = {}) {
+        let settings = super.initFetchSettings(customSettings);
+
+        const app = getOwner(injections);
+        const session = app.lookup('service:session');
+
+        if (session.isAuthenticated) {
+          let { access_token } = session.data.authenticated;
+          if (isPresent(access_token)) {
+            settings.headers['Authorization'] = `Bearer ${access_token}`;
+          }
+        }
+
+        settings.headers['Content-Type'] = 'application/vnd.api+json';
+        delete settings.headers['Accept'];
+
+        return settings;
+      }
+    }
+    injections.name = 'remote';
+    injections.host =
+      'https://jpuj8ukmx8.execute-api.us-east-2.amazonaws.com/dev';
+    injections.namespace = 'api';
+    injections.RequestProcessorClass = RemoteRequestProcessor;
+
+    return new RemoteSource(injections);
+  }
+};

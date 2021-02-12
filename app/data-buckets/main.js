6022289ca422ae32d665e8adcfe941078d4ad687
Robert Wagner <rwwagner90@gmail.com>
2021-02-12 16:25:37 -0500

    Add to CSP, remove bucket

--- app/data-buckets/main.js
+++
@@ -1,9 +0,0 @@
-import BucketClass from '@orbit/indexeddb-bucket';
-
-export default {
-  create(injections = {}) {
-    injections.name = 'main';
-    injections.namespace = 'swach-main';
-    return new BucketClass(injections);
-  }
-};
--- config/content-security-policy.js
+++ config/content-security-policy.js
@@ -10,7 +10,11 @@ module.exports = function (environment) {
       'script-src': ['http://localhost:7020', "'self'", "'unsafe-inline'"],
       'font-src': ["'self'"],
       'frame-src': ["'self'"],
-      'connect-src': ["'self'"],
+      'connect-src': [
+        'https://cognito-idp.us-east-2.amazonaws.com/',
+        'https://jpuj8ukmx8.execute-api.us-east-2.amazonaws.com/dev/',
+        "'self'"
+      ],
       'img-src': ['data:', "'self'"],
       'style-src': ["'self'", "'unsafe-inline'"],
       'media-src': ["'self'"]

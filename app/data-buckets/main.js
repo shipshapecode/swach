e7127104495864c435267a6536a42585e7e89324
Robert Wagner <rwwagner90@gmail.com>
2021-03-08 12:40:00 -0500

    Fix lint

--- app/data-buckets/main.js
+++ app/data-buckets/main.js
@@ -3,8 +3,7 @@ import BucketClass from '@orbit/indexeddb-bucket';
 export default {
   create(injections = {}) {
     injections.name = 'main';
-    injections.namespace =
-      'swach-main';
+    injections.namespace = 'swach-main';
     return new BucketClass(injections);
   }
 };

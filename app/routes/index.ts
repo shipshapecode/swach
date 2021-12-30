540d6380cd76926385bc6fb0ac9be69afafb1ffe
Robert Wagner <rwwagner90@gmail.com>
2021-12-30 14:25:10 -0500

    ember-cli 4, bump some deps

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,7 +1,11 @@
 import Route from '@ember/routing/route';
+import Router from '@ember/routing/router-service';
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service router!: Router;
+
   beforeModel(): void {
-    this.replaceWith('palettes');
+    this.router.replaceWith('palettes');
   }
 }
--- app/routes/settings/cloud/index.ts
+++ app/routes/settings/cloud/index.ts
@@ -1,5 +1,6 @@
 import Transition from '@ember/routing/-private/transition';
 import Route from '@ember/routing/route';
+import Router from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 
 import CognitoService from 'ember-cognito/services/cognito';
@@ -7,14 +8,15 @@ import Session from 'ember-simple-auth/services/session';
 
 export default class SettingsAccountRoute extends Route {
   @service cognito!: CognitoService;
+  @service router!: Router;
   @service session!: Session;
 
   async beforeModel(transition: Transition): Promise<Transition> {
     if (!this.session.isAuthenticated) {
       transition.abort();
-      return this.transitionTo('settings.cloud.login');
+      return this.router.transitionTo('settings.cloud.login');
     }
 
-    return this.transitionTo('settings.cloud.profile');
+    return this.router.transitionTo('settings.cloud.profile');
   }
 }

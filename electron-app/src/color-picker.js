ba5105eeb47e637a0deb6602e15de30a3d2a2b9d
Robert Wagner <rwwagner90@gmail.com>
2022-01-04 21:00:59 -0500

    Refactor native EyeDropper usage

--- electron-app/src/color-picker.js
+++ electron-app/src/color-picker.js
@@ -1,25 +1,40 @@
-const { getColorHexRGB } = require('electron-color-picker');
-
 async function launchPicker(mb, type = 'global') {
-  getColorHexRGB()
-    .then((color) => {
-      mb.showWindow();
-      if (color) {
-        if (type === 'global') {
-          mb.window.webContents.send('changeColor', color);
-        }
-        if (type === 'contrastBg') {
-          mb.window.webContents.send('pickContrastBgColor', color);
-        }
-        if (type === 'contrastFg') {
-          mb.window.webContents.send('pickContrastFgColor', color);
-        }
-      }
-    })
-    .catch((error) => {
-      console.warn(`[ERROR] getColor`, error);
-      return '';
-    });
+  mb.hideWindow();
+
+  const color = await mb.window.webContents.executeJavaScript(
+    `async function openEyeDropper() {
+  const eyeDropper = new EyeDropper();
+
+  let color = '';
+
+  try {
+    const result = await eyeDropper.open();
+    color = result.sRGBHex;
+  } catch (error) {
+    console.warn(\`[ERROR] launchPicker EyeDropper\`, error);
+  }
+
+  return color;
+  }
+  
+  openEyeDropper();
+  `,
+    true
+  );
+
+  if (color) {
+    if (type === 'global') {
+      mb.window.webContents.send('changeColor', color);
+    }
+    if (type === 'contrastBg') {
+      mb.window.webContents.send('pickContrastBgColor', color);
+    }
+    if (type === 'contrastFg') {
+      mb.window.webContents.send('pickContrastFgColor', color);
+    }
+  }
+
+  mb.showWindow();
 }
 
 module.exports = {

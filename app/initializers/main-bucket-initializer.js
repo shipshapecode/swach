e9d019152981963de4815aefdf62115ab555bfd2
Robert Wagner <rwwagner90@gmail.com>
2022-01-24 09:47:33 -0500

    Bump deps, update orbit bucket

--- app/initializers/main-bucket-initializer.js
+++ app/initializers/main-bucket-initializer.js
@@ -1,30 +1,9 @@
 import BucketFactory from '../data-buckets/main';
 
 export function initialize(application) {
-  const config = application.resolveRegistration('ember-orbit:config') || {};
+  const orbitConfig = application.resolveRegistration('ember-orbit:config');
 
-  const bucketService = 'main-bucket';
-
-  // Register bucket service
-  application.register(`service:${bucketService}`, BucketFactory);
-
-  // Inject bucket into all sources
-  if (config.types) {
-    application.inject(
-      config.types.source,
-      'bucket',
-      `service:${bucketService}`
-    );
-  }
-
-  // Inject bucket into keyMap (if one is present)
-  if (config.services && !config.skipKeyMap) {
-    application.inject(
-      `service:${config.services.keyMap}`,
-      'bucket',
-      `service:${bucketService}`
-    );
-  }
+  application.register(`service:${orbitConfig.services.bucket}`, BucketFactory);
 }
 
 export default {

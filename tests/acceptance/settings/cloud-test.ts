91a9e93ee9cac55822b52371d874701371cd14da
Dan Gebhardt <dan@cerebris.com>
2022-02-24 08:19:55 -0500

    Add signup tests and remove data sync on signup
    Data cannot be sync'd with remote until the user has been authenticated, which happens upon login (not signup).

--- tests/acceptance/settings/cloud-test.ts
+++ tests/acceptance/settings/cloud-test.ts
@@ -2,7 +2,12 @@ import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
-import { mockCognitoUser } from 'ember-cognito/test-support';
+import {
+  MockAuth,
+  MockUser,
+  mockAuth,
+  mockCognitoUser
+} from 'ember-cognito/test-support';
 
 import sinon from 'sinon';
 
@@ -16,6 +21,61 @@ module('Acceptance | settings/cloud', function (hooks) {
     await visit('/settings/cloud/login');
 
     assert.strictEqual(currentURL(), '/settings/cloud/login');
+  });
+
+  test('user can sign up', async function (assert) {
+    assert.expect(5);
+
+    await mockAuth(
+      MockAuth.extend({
+        async signUp() {
+          assert.ok(true, 'signUp has been called');
+          return MockUser.create({
+            username: 'testuser@gmail.com',
+            attributes: {
+              sub: 'aaaabbbb-cccc-dddd-eeee-ffffgggghhhh',
+              email: 'testuser@gmail.com',
+              email_verified: 'false'
+            }
+          });
+        },
+        async confirmSignUp(username: string, confirmationCode: string) {
+          assert.strictEqual(
+            username,
+            'testuser@gmail.com',
+            'username is correct'
+          );
+          assert.strictEqual(
+            confirmationCode,
+            '1234',
+            'confirmationCode is correct'
+          );
+          return;
+        }
+      })
+    );
+
+    await visit('/settings/cloud/register');
+    await fillIn('[data-test-register-input-user]', 'testuser@gmail.com');
+    await fillIn('[data-test-register-input-password]', 'password');
+    await click('[data-test-register-submit]');
+    await waitForAll();
+
+    assert.strictEqual(
+      currentURL(),
+      '/settings/cloud/register/confirm',
+      'transitioned to register/confirm route'
+    );
+    await fillIn('[data-test-register-input-user]', 'testuser@gmail.com');
+    await fillIn('[data-test-register-input-code]', '1234');
+    await click('[data-test-register-submit]');
+    await waitForAll();
+
+    assert.strictEqual(
+      currentURL(),
+      '/settings/cloud/login',
+      'transitioned to login route'
+    );
   });
 
   test('user can login', async function (assert) {

1a0d4e2801413bedfadd216bcf6d8c7b59136ce7
Dan Gebhardt <dan@cerebris.com>
2022-03-01 17:19:11 -0500

    Remove unnecessary transition after logOut

--- app/controllers/settings/cloud/profile.ts
+++ app/controllers/settings/cloud/profile.ts
@@ -1,17 +1,14 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import Router from '@ember/routing/router-service';
 import { service } from '@ember/service';
 
 import Session from 'ember-simple-auth/services/session';
 
 export default class SettingsAccountController extends Controller {
-  @service router!: Router;
   @service session!: Session;
 
   @action
   logOut(): void {
     this.session.invalidate();
-    this.router.transitionTo('settings.cloud.login');
   }
 }

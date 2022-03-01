3d08e940df099dc82616cf158ffff157ccb4c018
Dan Gebhardt <dan@cerebris.com>
2022-03-01 14:58:49 -0500

    Introduce remote queryFail and updateFail strategies

---
+++ app/data-strategies/remote-updatefail.js
@@ -0,0 +1,23 @@
+import { RequestStrategy } from '@orbit/coordinator';
+import { NetworkError } from '@orbit/jsonapi';
+
+export default {
+  create() {
+    return new RequestStrategy({
+      name: 'remote-updatefail',
+      source: 'remote',
+      on: 'updateFail',
+      action(transform, e) {
+        // Retry network / fetch failures
+        if (e instanceof NetworkError || e.message === 'Failed to fetch') {
+          // TODO: Consider an incremental backoff rather than a fixed delay.
+          setTimeout(() => {
+            this.source.requestQueue.retry();
+          }, 5000);
+        } else {
+          // TODO: Consider logging additional errors with sentry or equivalent.
+        }
+      }
+    });
+  }
+};

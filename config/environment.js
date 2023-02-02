119360d37c39e48ce6bd4a01eefa086901920b34
Robert Wagner <rwwagner90@gmail.com>
2023-02-02 09:07:24 -0500

    ember-cli 4.10

--- config/environment.js
+++ config/environment.js
@@ -3,12 +3,13 @@
 const version = require('../package').version;
 
 module.exports = function (environment) {
-  let ENV = {
+  const ENV = {
     modulePrefix: 'swach',
     environment,
     rootURL: process.env.EMBER_CLI_ELECTRON ? '' : '/',
     locationType: process.env.EMBER_CLI_ELECTRON ? 'hash' : 'history',
     EmberENV: {
+      EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true

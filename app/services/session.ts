980236396dd461a4cca0740e7c4da795a647053a
Robert Wagner <rwwagner90@gmail.com>
2023-07-26 22:13:00 -0400

    Lint fix

--- app/services/session.ts
+++ app/services/session.ts
@@ -1,5 +1,7 @@
 import { inject as service } from '@ember/service';
+
 import BaseSessionService from 'ember-simple-auth/services/session';
+
 import type DataService from 'swach/services/data';
 
 export default class SessionService extends BaseSessionService {

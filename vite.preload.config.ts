5d38bf9e681aaf9cf60ce9e58663b745f010d761
Robert Wagner <rwwagner90@gmail.com>
2025-08-15 15:36:43 -0400

    Update configs

--- vite.preload.config.ts
+++ vite.preload.config.ts
@@ -1,4 +1,15 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'esm',
+        entryFileNames: '[name].mjs',
+        chunkFileNames: '[name].mjs',
+      },
+    },
+    sourcemap: true,
+  },
+});

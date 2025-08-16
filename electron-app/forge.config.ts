3c2534184a4267e86f7eecae2be9ba80f1a35376
Robert Wagner <rwwagner90@gmail.com>
2025-08-16 09:44:58 -0400

    Sync up vite versions

--- electron-app/forge.config.ts
+++ electron-app/forge.config.ts
@@ -120,13 +120,13 @@ const config: ForgeConfig = {
       build: [
         {
           // `entry` is just an alias for `build.lib.entry` in the corresponding file of `config`.
-          entry: 'src/main.ts',
-          config: 'vite.main.config.ts',
+          entry: 'electron-app/src/main.ts',
+          config: '../vite.main.config.ts',
           target: 'main',
         },
         {
           entry: 'electron-app/src/preload.ts',
-          config: 'vite.preload.config.ts',
+          config: '../vite.preload.config.ts',
           target: 'preload',
         },
       ],

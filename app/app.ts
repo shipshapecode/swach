7a2941c06be59f073e527ab73b28030c2d0f3a96
Robert Wagner <rwwagner90@gmail.com>
2025-10-23 11:15:36 -0400

    Update app.ts

--- app/app.ts
+++ app/app.ts
@@ -25,6 +25,7 @@ if (macroCondition(!isTesting())) {
       environment: config.environment,
       release: `v${pkg.version}`,
     },
+    // @ts-expect-error TODO: fix this type error
     emberInit
   );
 }

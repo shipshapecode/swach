14d3ddb7628d65221f567018adc55e9f05721611
Robert Wagner <rwwagner90@gmail.com>
2025-10-30 22:22:54 -0400

    Bump some deps

--- app/deprecation-workflow.ts
+++ app/deprecation-workflow.ts
@@ -10,6 +10,7 @@ setupDeprecationWorkflow({
   */
   throwOnUnhandled: false,
   workflow: [
+    { handler: 'silence', matchId: 'deprecate-import-testing-from-ember' },
     { handler: 'silence', matchId: 'ember-simple-auth.events.session-service' },
     { handler: 'silence', matchId: 'ember-polyfills.deprecate-assign' },
     { handler: 'silence', matchId: 'ember-string.htmlsafe-ishtmlsafe' },
